5e92d180d55f54f4a4ca33c48a88c57f54d26db7
Michał Kotfica <kotvitz17@gmail.com>
2023-09-09 13:44:06 +0200

    create controller

---
+++ src/main/java/com/example/githubrepositories/controller/GithubController.java
@@ -0,0 +1,29 @@
+package com.example.githubrepositories.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.githubrepositories.model.GithubRepository;
+import com.example.githubrepositories.service.GithubService;
+
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/github")
+public class GithubController {
+	
+    private final GithubService githubService;
+    
+    @Autowired
+    public GithubController(GithubService githubService) {
+        this.githubService = githubService;
+    }
+    
+    @GetMapping("/repositories/{username}")
+    public Flux<GithubRepository> listUserRepositories(@PathVariable String username) {
+        return githubService.getUserRepositories(username);
+    }
+}
