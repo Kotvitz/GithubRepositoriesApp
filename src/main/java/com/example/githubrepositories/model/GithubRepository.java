8a45a1bae6820ca1832305fbcf963f81245c46dc
Michał Kotfica <kotvitz17@gmail.com>
2023-09-10 18:29:36 +0200

    corrections in repository model

--- src/main/java/com/example/githubrepositories/model/GithubRepository.java
+++ src/main/java/com/example/githubrepositories/model/GithubRepository.java
@@ -2,13 +2,14 @@ package com.example.githubrepositories.model;
 
 import java.util.List;
 
-public record GithubRepository(String name, String owner, List<GithubBranch> branches) {
+public record GithubRepository(String name, GithubUser owner, List<GithubBranch> branches,
+		boolean fork) {
 	
 	public boolean isFork() {
-		return !owner.equals(name);
+		return fork;
 	}
 
 	public GithubRepository setBranches(List<GithubBranch> branches) {
-		return new GithubRepository(name, owner, branches);
+		return new GithubRepository(name, owner, branches, fork);
 	}
 }
