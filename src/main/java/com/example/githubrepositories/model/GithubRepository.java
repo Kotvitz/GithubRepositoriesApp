ed927be559c889ccae283c0854ac597dfb1199b2
Michał Kotfica <kotvitz17@gmail.com>
2023-09-11 10:33:35 +0200

    delete unnecessary field from repository record

--- src/main/java/com/example/githubrepositories/model/GithubRepository.java
+++ src/main/java/com/example/githubrepositories/model/GithubRepository.java
@@ -2,14 +2,9 @@ package com.example.githubrepositories.model;
 
 import java.util.List;
 
-public record GithubRepository(String name, GithubUser owner, List<GithubBranch> branches,
-		boolean fork) {
+public record GithubRepository(String name, GithubUser owner, boolean fork) {
 	
 	public boolean isFork() {
 		return fork;
-	}
-
-	public GithubRepository setBranches(List<GithubBranch> branches) {
-		return new GithubRepository(name, owner, branches, fork);
 	}
 }
